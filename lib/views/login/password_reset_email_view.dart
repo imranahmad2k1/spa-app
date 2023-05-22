0e7483a0a8198085da37d0b027d31155096b9b46
Imran Ahmad <meetimrang@gmail.com>
2023-05-22 17:35:13 +0500

    fixed minor vscode problems

--- lib/views/login/password_reset_email_view.dart
+++ lib/views/login/password_reset_email_view.dart
@@ -41,6 +41,5 @@ class PasswordResetEmailView extends StatelessWidget {
         ),
       ),
     );
-    ;
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:student_personal_assistant/main.dart';
+// import 'package:student_personal_assistant/main.dart'; //COMMENTED
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    // await tester.pumpWidget(const MyApp()); //COMMENTED
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
